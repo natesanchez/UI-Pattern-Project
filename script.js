f9ae490239f107ad2ab63892063d584ea187720c
Nate_Sanchez <NSanchez@usa.com>
2020-02-06 15:30:43 -0500

    api call and test

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 
 
-let p = document.querySelector("p")
+// let p = document.querySelector("p")
 
-someText = "It is a period of civil war.\r\nRebel spaceships, striking\r\nfrom a hidden base, have won\r\ntheir first victory against\r\nthe evil Galactic Empire.\r\n\r\nDuring the battle, Rebel\r\nspies managed to steal secret\r\nplans to the Empire's\r\nultimate weapon, the DEATH\r\nSTAR, an armored space\r\nstation with enough power\r\nto destroy an entire planet.\r\n\r\nPursued by the Empire's\r\nsinister agents, Princess\r\nLeia races home aboard her\r\nstarship, custodian of the\r\nstolen plans that can save her\r\npeople and restore\r\nfreedom to the galaxy....".replace(/(\r\n|\n|\r)/gm, " ");
+// someText = "It is a period of civil war.\r\nRebel spaceships, striking\r\nfrom a hidden base, have won\r\ntheir first victory against\r\nthe evil Galactic Empire.\r\n\r\nDuring the battle, Rebel\r\nspies managed to steal secret\r\nplans to the Empire's\r\nultimate weapon, the DEATH\r\nSTAR, an armored space\r\nstation with enough power\r\nto destroy an entire planet.\r\n\r\nPursued by the Empire's\r\nsinister agents, Princess\r\nLeia races home aboard her\r\nstarship, custodian of the\r\nstolen plans that can save her\r\npeople and restore\r\nfreedom to the galaxy....".replace(/(\r\n|\n|\r)/gm, " ");
 
-p.innerText = someText
+// p.innerText = someText
+
+
+
+
+let BaseUrl = "https://swapi.co/api/films/"
+
+function fetchCrawl () {
+fetch(BaseUrl, ).then(res => {
+    return res.json();
+  })
+  .then(res => {
+    console.log(res);
+  })
+  .catch(err => {
+    console.log("something went wrong...", err);
+  });
+}
+
+fetchCrawl();
